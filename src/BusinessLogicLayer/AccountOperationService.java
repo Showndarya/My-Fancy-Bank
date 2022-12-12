db5c9d00224461a4d2467ec66a192b6d410d55b4
Showndarya <shmadhav@bu.edu>
2022-12-11 23:00:09 -0500

    Add getBalance method

--- src/BusinessLogicLayer/AccountOperationService.java
+++ src/BusinessLogicLayer/AccountOperationService.java
@@ -5,4 +5,5 @@ import Enums.TransactionType;
 public interface AccountOperationService {
 
     public Boolean changeBalance(TransactionType type, int accountId, double amount);
+    public double getBalance(int accountId);
 }
