9086cbbf1239aabaffa6318ac926fa260e88b373
weirande2009 <1209382452@qq.com>
2022-12-11 23:54:26 -0500

    AccountDao

--- src/DataAccessLayer/Implementation/SavingsAccountDaoImpl.java
+++ src/DataAccessLayer/Implementation/SavingsAccountDaoImpl.java
@@ -1,57 +1,80 @@
 package DataAccessLayer.Implementation;
 
+import DataAccessLayer.BaseDao;
 import DataAccessLayer.Interfaces.SavingsAccountDao;
 import Enums.AccountType;
 import Models.Account.Account;
-import Models.MoneyType;
+import Models.Account.CheckingAccount;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
+import java.sql.Statement;
 
 public class SavingsAccountDaoImpl implements SavingsAccountDao {
     @Override
     public Account getById(int id) throws SQLException {
-        return null;
+        Connection connection = BaseDao.getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String sql = "select * from account " +
+                "where id = " + id + ";";
+        resultSet = BaseDao.execute(connection, sql, statement, resultSet);
+        CheckingAccount checkingAccount = null;
+        if(resultSet.next()){
+            checkingAccount = new CheckingAccount();
+            checkingAccount.setOwnerId(resultSet.getInt("id"));
+            checkingAccount.setType(AccountType.Savings);
+            // Set money
+//            checkingAccount.setMoney();
+        }
+        return checkingAccount;
     }
 
     @Override
-    public boolean hasAccount(int ownerId, AccountType type) throws SQLException {
-        return false;
+    public boolean hasAccount(int ownerId) throws SQLException {
+        Connection connection = BaseDao.getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String sql = "select id from account " +
+                "where user_id = " + ownerId + " and account_type = " + AccountType.Savings + ";";
+        resultSet = BaseDao.execute(connection, sql, statement, resultSet);
+        return resultSet.next();
     }
 
     @Override
-    public Account getByOwnerIdAndType(int ownerId, AccountType type) throws SQLException {
-        return null;
+    public Account getByOwnerId(int ownerId) throws SQLException {
+        Connection connection = BaseDao.getConnection();
+        Statement statement = null;
+        ResultSet resultSet = null;
+        String sql = "select * from account " +
+                "where user_id = " + ownerId + " and account_type = " + AccountType.Savings + ";";
+        resultSet = BaseDao.execute(connection, sql, statement, resultSet);
+        CheckingAccount checkingAccount = null;
+        if(resultSet.next()){
+            checkingAccount = new CheckingAccount();
+            checkingAccount.setOwnerId(resultSet.getInt("id"));
+            checkingAccount.setType(AccountType.Savings);
+            // Set money
+//            checkingAccount.setMoney();
+        }
+        return checkingAccount;
     }
 
     @Override
-    public int addAccount(int ownerId, AccountType type) throws SQLException {
-        return 0;
+    public int addAccount(Connection connection, int ownerId) throws SQLException {
+        Statement statement = null;
+        String sql = "insert into account ('user_id', 'account_type') values ("+ ownerId + ","+ AccountType.Savings + ");";
+        int affectRows = BaseDao.executeUpdate(connection, sql, statement);
+        return affectRows;
     }
 
     @Override
-    public int removeAccount(int id) throws SQLException {
-        return 0;
-    }
-
-    @Override
-    public int modifyAccountMoney(Connection connection, int id, MoneyType type, double money) throws SQLException {
-        return 0;
-    }
-
-    @Override
-    public HashMap<Integer, Double> getAllKindsOfMoney(int account_id) throws SQLException {
-        return null;
-    }
-
-    @Override
-    public double getMoney(MoneyType type, int account_id) throws SQLException {
-        return 0;
-    }
-
-    @Override
-    public int setMoney(MoneyType type, int account_id) throws SQLException {
-        return 0;
+    public int removeAccount(Connection connection, int id) throws SQLException {
+        Statement statement = null;
+        String sql = "delete from account " +
+                "where id = " + id + ";";
+        int affectRows = BaseDao.executeUpdate(connection, sql, statement);
+        return affectRows;
     }
 }
